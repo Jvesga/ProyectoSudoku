a72b04b5453245ccb060c68a443ca815a0a27d7e
Jorge Alejandro Vesga Quintero <joalvesgaqu@gmail.com>
2022-12-03 01:30:14 -0500

    .

---
+++ Ejecutable/Test.java
@@ -0,0 +1,14 @@
+package Ejecutable;
+
+import modelo.Sudoku;
+import vista.VentanaPrincipal;
+
+public class Test 
+{
+    public static void main(String[] args) 
+    {
+        VentanaPrincipal miVentana = new VentanaPrincipal();
+        Sudoku misSudoku = new Sudoku();
+    }
+
+}
--- modelo/Sudoku.java
+++ modelo/Sudoku.java
@@ -1,4 +1,4 @@
-package Modelo;
+package modelo;
 
 import java.util.Random;
 import javax.swing.JTextField;
